8722fc151981b874a398b5d2eca4dcff13b8bb40
Igor <yaligor199@gmail.com>
2019-09-12 19:34:06 +0300

    Initial commit

---
+++ src/main/java/igor/ReceiptDB/ReceiptDbApplication.java
@@ -0,0 +1,13 @@
+package igor.ReceiptDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptDbApplication.class, args);
+	}
+
+}
